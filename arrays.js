3ed19b04fa85f3c24a5fe3bc3fc0cefaedaa34a9
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2024-02-06 16:18:24 -0500

    Add code along and solo

---
+++ arrays.js
@@ -0,0 +1,39 @@
+// CODE ALONG
+
+// 1. Update the variable songList so that it is an array with at least 3 song titles.
+let songList;
+console.log();
+
+// 2. Update the variable studentList so that it is an array with 4 Code Nation students (including yourself) in your class.
+let studentList;
+console.log();
+
+// 3. Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
+let teacherList;
+console.log();
+
+// 4. Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
+let familyAges;
+console.log();
+
+//PAUSE HERE
+
+// CODE SOLO
+
+// 5. Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
+let favSong;
+console.log();
+
+// 6. Update the variable bestStudent to retrieve your name from the classList array (Task 2).
+let bestStudent;
+console.log();
+
+// 7. Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
+let tallestTeacher;
+console.log();
+
+// 8. Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
+let oldestMember;
+console.log();
+
+
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// TODO
+// Scenario:
+// You're building an application for a music shop. They want a feature that 
+// recommends an song based on their current mood.
+
+// 1. Create a variable named mood.
+//  - Assign it a string value of "happy".
+
+
+// 2. Create a variable named recommendedSong. Do not assign a value.
+
+
+// 3. Implement the recommendation logic.
+// - If the mood is equal to "happy", recommend "Here Comes the Sun by The Beatles".
+// - If the mood is equal to "sad", recommend "Someone Like You by Adele".
+// - Anything else, recommend "Break My Soul by Beyonce".
+
+
+// 4. Console log the recommended song.
